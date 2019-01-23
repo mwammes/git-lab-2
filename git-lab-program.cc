aeb5d35e67ae629843c1995b5af998efdfffaecf
Meredith Wammes <mw270117@ohio.edu>
2019-01-22 19:17:47 -0500

    Final Commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Meredith Wammes
+ *        Date: January 22 2019
+ * Description: Lab 2 Hello Git Code
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
